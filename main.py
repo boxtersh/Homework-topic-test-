4d931a56fdc8878023780b15d225e88383291973
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-15 13:12:34 +0300

    feat function (is_palindrome)

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+def __check_parameter_for_str(text: str)->None:
+
+    if not isinstance(text, str):
+        raise TypeError(f'Не соответствие типов, ожидалось <class str>, получили {type(text)}')
+
+def __check_parameter_for_list(lst: list)->None:
+    if not isinstance(lst, list):
+        raise TypeError(f'Не соответствие типов, ожидалось <class list>, получили {type(lst)}')
+
 # *************** Тестирование ДЗ №1 *********************************************
 def find_substr(text: str, pattern: str) -> int:
     """
-    Поиск первого вхождения (индекс)
-    подстроки pattern в строке text
+    Поиск первого вхождения (индекс) подстроки pattern в строке text
     Пример:
     text = "hello, world!"
     pattern = "world"
@@ -12,6 +20,9 @@ def find_substr(text: str, pattern: str) -> int:
     :param pattern: Подстрока
     :return: индекс первого вхождения
     """
+    __check_parameter_for_str(text)
+    __check_parameter_for_str(pattern)
+
     return text.find(pattern)
 
 # *************** Тестирование ДЗ №2 *********************************************
@@ -19,19 +30,60 @@ def find_elem(lst: list, value) -> int:
     """
     Поиск первого искомого элемента в списке
     Пример:
-    lst = [1,2,3,1,4,5], value = 2
+    lst = [1,2,3,1,4,5]
+    value = 2
     -> 1
     :param lst: список
     :param value: элемент
     :return: индекс найденного элемента
     """
+    __check_parameter_for_list(lst)
+
     if value in lst:
         return lst.index(value)
 
     return -1
 
 # *************** Тестирование ДЗ №3 *********************************************
+def count_occurrences(lst: list, value) -> int:
+    """
+    Поиск количества вхождений искомого элемента в списке
+    Пример:
+    lst = [1,2,3,1,4,3]
+    value = 3
+    -> 2
+    :param lst: список
+    :param value: элемент
+    :return: количество вхождений
+    """
+    __check_parameter_for_list(lst)
+
+    if value in lst:
+        return lst.count(value)
+
+    return -1
 
 # *************** Тестирование ДЗ №4 *********************************************
+def reverse_words(s: str) -> str:
+    """
+    Переставляет строку из слов в обратном порядке:
+    Пример:
+    s = one two three
+    -> three two one
+    :param s: строка
+    :return: реверс строки
+    """
+    __check_parameter_for_str(s)
+
+    return ' '.join(s.split(' ')[::-1])
 
 # *************** Тестирование ДЗ №5 *********************************************
+def is_palindrome(s: str) -> bool:
+
+    __check_parameter_for_str(s)
+
+    s_nwe = s.replace(' ','')
+
+    return s_nwe.lower()==s_nwe.lower()[::-1]
+
+# ********************************************************************************
---
+++ tests/test_HW.py
@@ -0,0 +1,147 @@
+import pytest
+import main
+
+# *************** Тестирование ДЗ №1 *********************************************
+def test_positive_find_substr():
+
+    text = 'hello, world!'
+    pattern = 'world'
+    expected = 7
+
+    result = main.find_substr(text, pattern)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_find_substr_line_empty():
+
+    text = ''
+    pattern = 'world'
+    expected = -1
+
+    result = main.find_substr(text, pattern)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_typeerror_param_text():
+
+    text = 13
+    pattern = 'world'
+
+    with pytest.raises(TypeError,f'Ожидалось: <class str>, получили: {type(text)}'):
+        main.find_substr(text, pattern)
+
+
+def test_typeerror_param_pattern():
+
+    text = 'hello, world!'
+    pattern = 3
+
+    with pytest.raises(TypeError,f'Ожидалось: <class str>, получили: {type(pattern)}'):
+        main.find_substr(text, pattern)
+
+
+# *************** Тестирование ДЗ №2 *********************************************
+def test_find_yes_elem():
+
+    lst = [1,2,3,2,5,2,3,5,4]
+    value = 5
+    expected = 4
+
+    result =  main.find_elem(lst, value)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_find_no_value():
+
+    lst = [1,2,3,2,2,3,1,4,6]
+    value = 5
+    expected = -1
+
+    result =  main.find_elem(lst, value)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_find_lst_empty():
+
+    lst = []
+    value = 5
+    expected = -1
+
+    result =  main.find_elem(lst, value)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_parameter_not_list():
+
+    lst = 4
+    value = 5
+
+    with pytest.raises(TypeError,f'Ожидалось: <class list>, получили: {type(lst)}'):
+        main.find_elem(lst, value)
+
+# *************** Тестирование ДЗ №3 *********************************************
+def test_count_yes_value():
+
+    lst = [1,2,3,2,5,2,3,5,3]
+    value = 3
+    expected = 3
+
+    result =  main.count_occurrences(lst, value)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_count_no_value():
+
+    lst = [1,2,3,2,2,3,1,4,6]
+    value = 5
+    expected = -1
+
+    result =  main.count_occurrences(lst, value)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_count_lst_empty():
+
+    lst = []
+    value = 5
+    expected = -1
+
+    result =  main.count_occurrences(lst, value)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_count_parameter_not_list():
+
+    lst = 4
+    value = 5
+
+    with pytest.raises(TypeError,f'Ожидалось: <class list>, получили: {type(lst)}'):
+        main.count_occurrences(lst, value)
+
+# *************** Тестирование ДЗ №4 *********************************************
+def test_reverse_positive():
+
+    s = 'one two three'
+    expected = 'three two one'
+
+    result = main.reverse_words(s)
+
+    assert result == expected, f'Ожидалось: {expected}, получили: {result}'
+
+
+def test_reverse_parameter_not_str():
+
+    s = 4
+
+    with pytest.raises(TypeError,f'Ожидалось: <class str>, получили: {type(s)}'):
+        main.reverse_words(s)
+
+
